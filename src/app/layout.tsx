78bb65e63df2b2360d54f77db36c5a08b257c0b8
Your Name <your-email@example.com>
2026-07-15 16:42:55 -0500

    add favicon link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/icons/icon.svg" />
         <link rel="apple-touch-icon" href="/icons/icon.svg" />
         <link rel="apple-touch-startup-image" href="/icons/icon.svg" />
       </head>
